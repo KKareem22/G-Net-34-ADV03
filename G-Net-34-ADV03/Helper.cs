88cfb75e100521a4affb06e443fa5644c216f3d3
Kareem <krda4969@gmail.com>
2026-03-30 22:43:04 +0200

    Q01

---
+++ G-Net-34-ADV03/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Net_34_ADV03
+{
+    internal class Helper
+    {
+        public static void Print<T>(string name,IEnumerable<T>list)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"{name} : [ {string.Join(",",list)} ]");
+            Console.ResetColor();
+        }
+    }
+}
--- G-Net-34-ADV03/Program.cs
+++ G-Net-34-ADV03/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q01
+            //List<int> grades = [85, 92, 78, 95, 88, 70, 100, 65];
+            //Helper.Print("Grades", grades);
+            //Console.WriteLine($"Count : {grades.Count}");
+            //Console.WriteLine($"The First grade in grades ={grades.First()}");
+            //Console.WriteLine($"The Last grade in grades ={grades.Last()}");
+            //grades.Sort();
+            //Helper.Print("Sorted Grades Ascending", grades);
+            //Console.WriteLine($"The first grade above 90 ={grades.Find(x=>x>90)}");
+            //List<int> failingGrades = grades.FindAll(x => x < 75);
+            //Helper.Print("Failing Grades", failingGrades);
+            //grades.RemoveAll(x => x < 75);
+            //bool FullMarksExist = grades.Exists(x => x == 100);
+            //Console.WriteLine($"Is there any grade with full marks (100) ? {FullMarksExist}");
+            //List<string> FormattedGrades = new();
+            //foreach(var grade in grades)
+            //{
+            //    string item = $"Grade : {grade}";
+            //    FormattedGrades.Add(item);
+            //}
+            //FormattedGrades.ForEach(x => Console.WriteLine(x));
+
+            #endregion
         }
     }
 }
