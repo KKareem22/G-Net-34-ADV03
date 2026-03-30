3f786cd30f8b4f79b32711a364a30b51b5c2c1ad
Kareem <krda4969@gmail.com>
2026-03-30 23:59:44 +0200

    Q04

--- G-Net-34-ADV03/Program.cs
+++ G-Net-34-ADV03/Program.cs
@@ -49,56 +49,83 @@
             //Helper.Print("Players after removing score 200", Players);
             #endregion
             #region Q03
-           // Dictionary<string, string> PhoneBook = new();
-           // PhoneBook.Add("Alice", "123-456-7890");
-           // PhoneBook.Add("Bob", "987-654-3210");
-           // PhoneBook.Add("Charlie", "555-555-5555");
-           // PhoneBook.Add("Kareem", "551-666-4535");
-           // Helper.PrintDictionary("Phone Book", PhoneBook);
-           // PhoneBook["Zaid"] = "111-111-1111";// Add Zaid
-           //// Helper.Print("Phone Book after updating Alice's number", PhoneBook);
-           //try
-           // {
-           //     PhoneBook.Add("Ali", "222-222-2222");
-           //     PhoneBook.Add("Ali", "222-222-2222");
-           // }
-           // catch (ArgumentException ex)
-           // {
-           //     Console.WriteLine($"Error: {ex.Message}");
-           // }
+            // Dictionary<string, string> PhoneBook = new();
+            // PhoneBook.Add("Alice", "123-456-7890");
+            // PhoneBook.Add("Bob", "987-654-3210");
+            // PhoneBook.Add("Charlie", "555-555-5555");
+            // PhoneBook.Add("Kareem", "551-666-4535");
+            // Helper.PrintDictionary("Phone Book", PhoneBook);
+            // PhoneBook["Zaid"] = "111-111-1111";// Add Zaid
+            //// Helper.Print("Phone Book after updating Alice's number", PhoneBook);
+            //try
+            // {
+            //     PhoneBook.Add("Ali", "222-222-2222");
+            //     PhoneBook.Add("Ali", "222-222-2222");
+            // }
+            // catch (ArgumentException ex)
+            // {
+            //     Console.WriteLine($"Error: {ex.Message}");
+            // }
 
-           // Helper.PrintDictionary("Phone Book after adding Ali", PhoneBook);
+            // Helper.PrintDictionary("Phone Book after adding Ali", PhoneBook);
 
-           // bool Result01=PhoneBook.TryAdd("Rawan", "333-333-3333");
-           // Console.WriteLine($"Trying to add Rawan : {Result01}");
-           // bool Result02 = PhoneBook.TryAdd("Rawan", "333-333-3333");
-           // Console.WriteLine($"Trying to add Rawan again : {Result02}");
+            // bool Result01=PhoneBook.TryAdd("Rawan", "333-333-3333");
+            // Console.WriteLine($"Trying to add Rawan : {Result01}");
+            // bool Result02 = PhoneBook.TryAdd("Rawan", "333-333-3333");
+            // Console.WriteLine($"Trying to add Rawan again : {Result02}");
 
-           // if (PhoneBook.ContainsKey("Bale"))
-           // {
-           //     Console.WriteLine($"Bale's number is {PhoneBook["Bale"]}");
-           // }
-           // else
-           // {
-           //     Console.WriteLine($"There is no contact named Bale in the phone book.");
-           // }
-           // Console.WriteLine(PhoneBook.GetValueOrDefault("Eman", "Not Found"));//Not Found is the default value if Eman is not found in the dictionary
+            // if (PhoneBook.ContainsKey("Bale"))
+            // {
+            //     Console.WriteLine($"Bale's number is {PhoneBook["Bale"]}");
+            // }
+            // else
+            // {
+            //     Console.WriteLine($"There is no contact named Bale in the phone book.");
+            // }
+            // Console.WriteLine(PhoneBook.GetValueOrDefault("Eman", "Not Found"));//Not Found is the default value if Eman is not found in the dictionary
 
-           // Console.WriteLine(new string('_',15));
-           // Console.WriteLine("____Keys____");
-           // foreach (var key in PhoneBook.Keys)
-           // {
-           //     Console.Write("  "+key);
-           // }
-           // Console.WriteLine("\n____Values____");
-           // foreach (var value in PhoneBook.Values)
-           // {
-           //     Console.Write("  "+value);
-           // }
-           // Console.WriteLine("\n"+new string('_', 15));
+            // Console.WriteLine(new string('_',15));
+            // Console.WriteLine("____Keys____");
+            // foreach (var key in PhoneBook.Keys)
+            // {
+            //     Console.Write("  "+key);
+            // }
+            // Console.WriteLine("\n____Values____");
+            // foreach (var value in PhoneBook.Values)
+            // {
+            //     Console.Write("  "+value);
+            // }
+            // Console.WriteLine("\n"+new string('_', 15));
 
             #endregion
+            #region Q04
+            //HashSet<string> EmailValidator = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            //EmailValidator.Add("ahmed@test.com");
+            //EmailValidator.Add("Ahmed@test.com");
+            //EmailValidator.Add("sara@test.com");
+            //EmailValidator.Add("Sara@Test.com");
+            //Console.WriteLine($"Count : {EmailValidator.Count}");//Count =2,Why? Because Only 2 unique emails due to case-insensitive comparison
 
+            //HashSet<int> SetA = [1,2, 3, 4, 5];
+            //HashSet<int> SetB = [4, 5, 6, 7, 8];
+            //Helper.Print("SetA", SetA);
+            //Helper.Print("SetB", SetB);
+            //// Union
+            //HashSet<int> Union = new(SetA);
+            //Union.UnionWith(SetB);
+            //Helper.Print("Union of SetA and SetB", Union);
+            //// Intersection
+            //HashSet<int> Intersection = new(SetA);
+            //Intersection.IntersectWith(SetB);
+            //Helper.Print("Intersection of SetA and SetB", Intersection);
+            //// Difference
+            //HashSet<int> Difference = new(SetA);
+            //Difference.ExceptWith(SetB);
+            //Helper.Print("Difference of SetA and SetB (SetA - SetB)", Difference);
+
+            //HashSet<int> subset = [1, 2];
+            //Console.WriteLine($"Is subset a subset of SetA ? {subset.IsSubsetOf(SetA)}");
+            #endregion
 
 
         }
